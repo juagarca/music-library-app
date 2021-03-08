77e2e049ab1b27fa8881dc845398bf3d4b71c442
Juan Garcia <juagarca@gmail.com>
2021-03-08 15:26:23 +0000

    Artist model associations and validations

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,2 +1,8 @@
 class Artist < ApplicationRecord
+  has_many :libraries
+  has_many :collaborations
+  has_many :albums
+  has_many :songs, through: :collaborations, as: :collaborations
+
+  validates :name, presence: true
 end
