4ebc175d9611424392d848fe910b091e4d85b1a7
Juan Garcia <juagarca@gmail.com>
2021-03-10 12:34:48 +0000

    artists and what's out today section addition to dashboard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,5 +6,18 @@ class PagesController < ApplicationController
 
   def dashboard
     @artists = current_user.artists
+    @albums_to_listen = find_albums_to_listen(@artists)
+  end
+
+  private
+
+  def find_albums_to_listen(artists)
+    albums = []
+    artists.each do |artist|
+      albums << artist.albums.select do |album|
+        album.release_date <= Date.today
+      end
+    end
+    albums.flatten
   end
 end
