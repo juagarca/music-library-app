2c49133a1eee45553c1f08e7d531db40a971e2a8
Juan Garcia <juagarca@gmail.com>
2021-03-12 13:09:55 +0000

    bio addition to search and result page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,5 @@
-
+require 'open-uri'
+require 'Nokogiri'
 
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: %i[home]
@@ -21,13 +22,20 @@ class PagesController < ApplicationController
   end
 
   def scraping
-    artist = params[:artist][:name]
-    redirect_to "/result/#{artist}"
+    artist = params[:artist][:url]
+    instagram = params[:artist][:instagram]
+    bio = params[:artist][:bio]
+
+    redirect_to "/result/#{artist}?instagram=#{instagram}&bio=#{bio}"
   end
 
   def result
     artist = params[:name]
+    @instagram = "https://www.instagram.com/#{params[:instagram]}"
     @url = "https://www.allmusic.com/artist/#{artist}"
+
+    @info = retrieve_info_from_web(@url)
+    @bio = params[:bio]
   end
 
   private
@@ -37,5 +45,28 @@ class PagesController < ApplicationController
     records.map do |record|
       record.album
     end
+  end
+
+  # Using AllMusic website
+  def retrieve_info_from_web(url)
+    html_file = open(url).read
+    html_doc = Nokogiri::HTML(html_file)
+    result = {}
+
+    # Artist
+    result[:name] = html_doc.search('.artist-name').text.strip
+    result[:description] = html_doc.search('.biography span').first.text.strip
+    result[:photo] = html_doc.search('.artist-contain img').attribute('src').value
+
+    # Performer
+    unless html_doc.search('.aliases div').first.nil?
+      result[:full_name] = html_doc.search('.aliases div').first.text.strip
+    else
+      result[:full_name] = result[:name]
+    end
+    result[:date_of_birth] = Date.parse(html_doc.search('.birth a').first.text.strip)
+    result[:birth_location] = html_doc.search('.birth a')[1].text.strip
+
+    result
   end
 end
